99e47faac60174450381d1d77b278e7d1b2db74d
Nathaniel Mitchell (Admin) <Nmitchell.admin@british-business-bank.co.uk>
2024-08-19 18:53:54 +0100

    Created Hide from GAL Powershell Module

---
+++ Hide_From_GAL.psm1
@@ -0,0 +1,30 @@
+﻿function GALHideBBB{
+param ([String]$username)
+
+Set-ADUser -Identity $username -Replace @{msExchHideFromAddressLists = $true}
+write-host "User hidden from GAL"
+#check if user is hidden from GAL
+$user = Get-ADUser -Filter{SamAccountname -eq $username} -Properties msExchHideFromAddressLists
+
+if ($user.msExchHideFromAddressLists -eq $true) {
+    [System.Windows.Forms.MessageBox]::Show("$username hidden from GAL", "Successfully hidden from GAL")
+} else {
+    [System.Windows.Forms.MessageBox]::Show("Error occured when hiding $username from GAL", "Error hiding from GAL")
+}
+
+} 
+
+function GALHideSULCO {
+param ([String]$username)
+$domainController = "startuploans.local"
+
+Set-ADUser -Identity $accountName.SamAccountName -Replace @{msExchHideFromAddressLists = $true} -Server $domainController
+#check if user is hidden from GAL
+$user = Get-ADUser -Filter{SamAccountname -eq $accountName.SamAccountName} -Properties msExchHideFromAddressLists -Server $domainController
+
+if ($user.msExchHideFromAddressLists -eq $true) {
+     [System.Windows.Forms.MessageBox]::Show("$username hidden from GAL", "Successfully hidden from GAL")
+} else {
+     [System.Windows.Forms.MessageBox]::Show("Error occured when hiding $username from GAL", "Error hiding from GAL")
+}
+}

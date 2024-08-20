c17d48edff5acd5174e2dcc0e575dbc7af2a4ecb
Nathaniel Mitchell (Admin) <Nmitchell.admin@british-business-bank.co.uk>
2024-08-20 18:28:03 +0100

    Fixed issue with error message in GALHIDESULCO

--- Hide_From_GAL.psm1
+++ Hide_From_GAL.psm1
@@ -26,21 +26,34 @@
 }
 
 
-
-
 function GALHideSULCO {
 param ([String]$username)
+
+$operationSuccess = $false  # Initialize a flag for operation success
+
 $domainController = "startuploans.local"
 
-write-Host $username , $domainController
 
-Set-ADUser -Identity $username -Replace @{msExchHideFromAddressLists = $true} -Server $domainController
-#check if user is hidden from GAL
-$user = Get-ADUser -Filter{SamAccountname -eq $username} -Properties msExchHideFromAddressLists -Server $domainController
+    try {Set-ADUser -Identity $username -Replace @{msExchHideFromAddressLists = $true} -Server $domainController
 
-if ($user.msExchHideFromAddressLists -eq $true) {
-     [System.Windows.Forms.MessageBox]::Show("$username hidden from GAL", "Successfully hidden from GAL")
-} else {
-     [System.Windows.Forms.MessageBox]::Show("Error occured when hiding $username from GAL", "Error hiding from GAL")
+    Set-ADUser -Identity $username -Replace @{msExchHideFromAddressLists = $true} -Server $domainController
+    #check if user is hidden from GAL
+    $user = Get-ADUser -Filter{SamAccountname -eq $username} -Properties msExchHideFromAddressLists -Server $domainController
+
+    if ($user.msExchHideFromAddressLists -eq $true) {
+         Write-Host "$username is hidden from address lists."
+         $operationSuccess = $true  # Set the flag to true if the operation succeeds
+    } else {
+         throw "Failed to hide $username from the Global Address List."
+    }
+    }catch {
+            # Handle any errors that occur during the process
+            Write-Host "An error occurred while trying to hide $username from the GAL: $($_.Exception.Message)"
+            throw  # Re-throw the exception to be caught by the main script
+        }
+
+return $operationSuccess  # Return the success flag
+
 }
-}
+
+
